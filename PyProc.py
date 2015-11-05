549f3fc23c64ceb548037253c2170d2ac7e2a3f3
holybasil <benjaminsw@live.com>
2015-11-05 22:07:42 +0000

    update createMetadata func
    update createMetadata func to call update createMetadataCSV func or
    update createMetadataJSON func

--- PyProc.py
+++ PyProc.py
@@ -1,13 +1,18 @@
 import os
 import sys
 
+def createMetadataCSV(fname):
+    #will update soon
 
-def createMetadata(fname, ext):
+def createMetadataJSON(fname):
     #will update soon
 
 
-
-
+def createMetadata(fname, ext):
+    if ext == '.csv':
+        createMetadataCSV(fname=fname)
+    elif ext == '.json':
+        createMetadataJSON(fname=fname)
 
 def main():
     #check if file exists
@@ -15,13 +20,16 @@ def main():
         with open(sys.argv[1]) as file:
             pass
     except IOError:
-        print('There was an error opening the file!')
+        print 'There was an error opening the file!'
         return
     acceptedFormat = ('.csv', '.txt', '.json')
     # Split the extension from the path and normalise it to lowercase.
-    inoutFile = os.path.splitext(sys.argv[1].lower())
-    ext = inoutFile[-1]
+    inputFile = os.path.splitext(sys.argv[1].lower())
+    ext = inputFile[-1]
     fname = sys.argv[1]
+    if ext not in acceptedFormat:
+        print 'Please input "CSV", "TSV" or "JSON"'
+        return
     createMetadata(fname=fname, ext=ext)
 
 if __name__ == '__main__':
