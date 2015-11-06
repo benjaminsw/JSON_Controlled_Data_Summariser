937cf196e9195e10e76d7bdcec87ceb7381716d5
holybasil <benjaminsw@live.com>
2015-11-06 15:53:42 +0000

    check file
    check if file exist and whether or not it is a json. if passes, read in

--- PyProc.py
+++ PyProc.py
@@ -1,21 +1,20 @@
+import json
+import os
+import sys
 
 def main():
-    #check if file exists
+    #check if file exists and wheter or not it is a json file
     try:
-        with open(sys.argv[1]) as file:
-            pass
+        with open(sys.argv[1]) as jsonfile:
+            jdata = json.load(jsonfile)
     except IOError:
         print 'There was an error opening the file!'
         return
-    acceptedFormat = ('.csv', '.txt', '.json')
-    # Split the extension from the path and normalise it to lowercase.
-    inputFile = os.path.splitext(sys.argv[1].lower())
-    ext = inputFile[-1]
-    fname = sys.argv[1]
-    if ext not in acceptedFormat:
-        print 'Please supply "CSV", "TSV" or "JSON" file'
+    except ValueError:
+        print 'The file was not in json format!'
         return
-    createmetadata(fname=fname, ext=ext)
+    print(jdata)
+    #createmetadata(fname=fname, ext=ext)
 
 if __name__ == '__main__':
     main()
