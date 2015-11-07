353caeacccbe61d837f3861c343efc48c3546982
holybasil <benjaminsw@live.com>
2015-11-07 13:25:16 +0000

    finished demo
    done all basic req but need an improvement to do more advanced stuff

--- PyProc.py
+++ PyProc.py
@@ -1,9 +1,10 @@
-import json
-import os
 import sys
 import re
+import json
+import collections
 
 def isint(value):
+
   try:
     int(value)
     return True
@@ -11,15 +12,26 @@ def isint(value):
     return False
 
 def toint(string):
+
     try:
         return int(string)
     except ValueError:
         return string
 
+def toascii(string):
+
+    try:
+        return float(string)
+    except ValueError:
+        return string.encode('ascii','ignore')
+
 def describedata(header,data):
+
     summariseddata=[]
     for h in header:
         if str(type(data[header.index(h)][1])) ==  "<type 'int'>":
+            summariseddata.append({'name':h,'type':'numeric', 'min':min(data[header.index(h)]),'max':max(data[header.index(h)])})
+        if str(type(data[header.index(h)][1])) ==  "<type 'float'>":
             summariseddata.append({'name':h,'type':'numeric', 'min':min(data[header.index(h)]),'max':max(data[header.index(h)])})
         elif str(type(data[header.index(h)][1])) ==  "<type 'str'>":
             summariseddata.append({'name':h, 'type':'string', 'uniquevalues':len(set(data[header.index(h)]))})
@@ -51,29 +63,56 @@ def readCSVdata(infile, delimiter=','):
     coldata = map(list, zip(*rowdata))
     return  {'header':header, 'data':coldata, 'hasheader':hasheader}
 
-def createmetadataCSV(infile):
-    return readCSVdata(infile=infile,delimiter=',')
+def readJSONdata(infile):
 
+    jdata = []
+    with open(infile, 'r') as f:
+        for line in f:
+            jdata.append(json.loads(line))
 
-def createmetadataTXT(infile):
-    return readCSVdata(infile=infile,delimiter='\t')
+    rowdata=[]
+    for j in jdata:
+        val=[]
+        header = []
+        #lambda sort by keys in descending order http://stackoverflow.com/questions/20577840/python-dictionary-sorting-in-descending-order-based-on-values
+        for k, v in collections.OrderedDict(sorted(j.items(), key=lambda t: t[0])).iteritems():
+            val.append(toascii(v))
+            header.append(k)
+        rowdata.append(val)
+    coldata = map(list, zip(*rowdata))
+    return {'data':coldata, 'header':header, 'hasheader':True}
 
-
-def createmetadataJSON(infile):
-    print infile
-    #will update soon
 
 def createmetadata(infile, ext, outfile):
+
     if ext == 'csv':
-        data = createmetadataCSV(infile=infile)
+        data = readCSVdata(infile=infile,delimiter=',')
+        data['sep']=','
+        data['type']='tabular'
     elif ext == 'json':
-        createmetadataJSON(infile=infile)
+        data = readJSONdata(infile=infile)
+        data['sep']=':'
+        data['type']='json'
     elif ext == 'txt':
-        data = createmetadataTXT(infile=infile)
+        data = readCSVdata(infile=infile,delimiter='\t')
+        data['sep']='\t'
+        data['type']='tabular'
     summariseddata = describedata(header = data['header'], data = data['data'])
-    print summariseddata
+    metadata = {'filename':infile,
+                'format':{'type':data['type'],'sep':data['sep']},
+                'headerrow':int(data['hasheader']),
+                'numentries':len(data['data'][1]),
+                'numfields':len(data['header']),
+                'fields':summariseddata}
+    writetoJSON(metadata=metadata, outfile=outfile)
+
+def writetoJSON(metadata, outfile):
+
+    with open(outfile, 'w') as fp:
+        json.dump(metadata, fp)
 
 def main():
+    
     #check if file exists and wheter or not it is a json file
     try:
         with open(sys.argv[1]) as jsonfile:
@@ -86,11 +125,11 @@ def main():
         return
     locfile = jdata['infile']
     lstfileext = re.split(r'[.]',locfile)
-    #print lstfileext
     ext = lstfileext[-1]
     outfile = jdata['metafile']
-
     createmetadata(infile=locfile, ext=ext, outfile=outfile)
+    print 'The program has been successfully executed'
+    print 'Benjamin Wiriyapong SID:2426366'
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,101 +1,14 @@
-import os
-import sys
+import json
+from pprint import pprint
+data = []
+with open('D:/Dropbox/StirUni/ITNPBD2/assignment/data/cars.json','r') as f:
+    #for line in f.readlines():
+        #data = f.readline().strip()
+        #print data
+    #data = f.readline()
+#print(data)
 
-def isint(value):
-  try:
-    int(value)
-    return True
-  except:
-    return False
+    for line in f:
+        data.append(json.loads(line))
 
-def toint(string):
-    try:
-        return int(string)
-    except ValueError:
-        return string
-
-def createmetadataCSV(fname):
-
-    data=readCSVdata(fname)
-    summariseddata = describedata(header = data['header'], data = data['data'])
-    print {'filename':str(os.getcwd()+'\\'+fname),
-           'format':{'type':'tabular', 'sep':','},
-           'header':data['hasheader']}
-
-def describedata(header,data):
-    summariseddata=[]
-    for h in header:
-        if str(type(data[header.index(h)][1])) ==  "<type 'int'>":
-            summariseddata.append({'name':h,'type':'numeric', 'min':min(data[header.index(h)]),'max':max(data[header.index(h)])})
-        elif str(type(data[header.index(h)][1])) ==  "<type 'str'>":
-            summariseddata.append({'name':h, 'type':'string', 'uniquevalues':len(set(data[header.index(h)]))})
-    return summariseddata
-
-def readCSVdata(fname):
-
-    with open(fname, 'r') as f:
-        line1 = f.readline().strip().split(',')
-        line2 = f.readline().strip().split(',')
-        boolline1 = [isint(s) for s in line1 ]
-        boolline2 = [isint(s) for s in line2 ]
-        if not(boolline1 == boolline2):
-            header = line1
-            hasheader = True
-            line2 = [toint(s) for s in line2 ]
-            rowdata=[line2]
-        else:
-            header = ['Var'+str(i) for i in range(1,len(line1)+1)]
-            hasheader = False
-            line1 = [toint(s) for s in line1 ]
-            line2 = [toint(s) for s in line2 ]
-            rowdata = [line1,line2]
-        for line in f.readlines():
-            line = line.strip().split(',')
-            line = [toint(s) for s in line ]
-            rowdata.append(line)
-
-    coldata = map(list, zip(*rowdata))
-    return  {'header':header, 'data':coldata, 'hasheader':hasheader}
-
-'''
-        #l,name = line.strip().split(',')
-        #lines.append((l,name))
-'''
-
-
-
-def createmetadataJSON(fname):
-    print fname
-    #will update soon
-
-
-def createmetadata(fname, ext):
-    if ext == '.csv':
-        createmetadataCSV(fname=fname)
-    elif ext == '.json':
-        createmetadataJSON(fname=fname)
-
-def main():
-    #check if file exists
-    try:
-        with open(sys.argv[1]) as file:
-            pass
-    except IOError:
-        print 'There was an error opening the file!'
-        return
-    acceptedFormat = ('.csv', '.txt', '.json')
-    # Split the extension from the path and normalise it to lowercase.
-    inputFile = os.path.splitext(sys.argv[1].lower())
-    ext = inputFile[-1]
-    fname = sys.argv[1]
-    if ext not in acceptedFormat:
-        print 'Please supply "CSV", "TSV" or "JSON" file'
-        return
-    createmetadata(fname=fname, ext=ext)
-
-if __name__ == '__main__':
-    main()
-
-
-
-
+print data
